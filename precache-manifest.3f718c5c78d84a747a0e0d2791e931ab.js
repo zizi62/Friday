9fd03cd39d3af6ba8d0f32180b2e7f0ac5e305cf
Zoia <zizi2405@gmail.com>
2020-06-24 21:02:19 -0400

    Updates

---
+++ precache-manifest.3f718c5c78d84a747a0e0d2791e931ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc4e9792b61bfc024ad5f044fe77bec",
+    "url": "/Friday/index.html"
+  },
+  {
+    "revision": "01e25f91fe27a667e85e",
+    "url": "/Friday/static/css/main.93e81d1f.chunk.css"
+  },
+  {
+    "revision": "50a2cc5ece8fb07bdd2a",
+    "url": "/Friday/static/js/2.95092a94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Friday/static/js/2.95092a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e25f91fe27a667e85e",
+    "url": "/Friday/static/js/main.291b5225.chunk.js"
+  },
+  {
+    "revision": "e3ff599573564e6f0a2c",
+    "url": "/Friday/static/js/runtime-main.5b8fa669.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Friday/precache-manifest.3f718c5c78d84a747a0e0d2791e931ab.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Friday/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
