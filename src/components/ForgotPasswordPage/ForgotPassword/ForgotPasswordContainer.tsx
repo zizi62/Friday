49deb9c8d90e9f0a55b01240aba8d22da925c28f
mityaycho <mityaycho@gmail.com>
2020-07-02 23:47:32 +0300

    add logic to forgotPassword, Container,Reducer,api

--- src/components/ForgotPasswordPage/ForgotPassword/ForgotPasswordContainer.tsx
+++ src/components/ForgotPasswordPage/ForgotPassword/ForgotPasswordContainer.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import ForgotPassword from './ForgotPassword'
+import React from 'react';
+import ForgotPassword from './ForgotPassword';
+import ErrorMessage from '../../common/ErrorMessage/ErrorMessage';
 
+import { useSelector } from 'react-redux';
+import { AppStateType } from '../../../Redux/store';
 
 interface IForgotPasswordContainerProps {
 
-}
+};
+
 
 const ForgotPasswordContainer: React.FC<IForgotPasswordContainerProps> = () => {
-    return <div>
-        <h2>FORGOT PASSWORD</h2>
-        <ForgotPassword />
-    </div>
-}
 
-export default ForgotPasswordContainer
+	const error = useSelector((store: AppStateType) => store.forgotPasswordPage.error);
+
+	return (
+		<div>
+			<h2>FORGOT PASSWORD</h2>
+			{!!error && <ErrorMessage message={error} />}
+			<ForgotPassword error={error} />
+		</div>
+	);
+};
+
+export default ForgotPasswordContainer;
