878722e759272805e90dcdc19042fc1da98ea9b5
mityaycho <mityaycho@gmail.com>
2020-07-02 17:07:20 +0300

    add button to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,46 @@
+import React, { useState } from 'react';
+import style from './header.module.css';
 
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import style from './header.module.css'
-import { SIGN_IN_PATH, REGISTER_PATH, FORGOT_PASSWORD_PATH, SET_NEW_PASSWORD_PATH, PROFILE_PATH } from '../Routes/Routes'
+import { NavLink } from 'react-router-dom';
+import {
+	SIGN_IN_PATH,
+	REGISTER_PATH,
+	FORGOT_PASSWORD_PATH,
+	SET_NEW_PASSWORD_PATH,
+	PROFILE_PATH
+} from '../Routes/Routes';
 
 
 
 type HeaderPropsType = {
 
-}
+};
 
- const Header:React.FC<HeaderPropsType> = () =>{
+type setDisableMenuType = {
+	disableMenu: boolean;
+};
 
-    return <div className={style.header}>
-        <NavLink className={style.link} to={SIGN_IN_PATH}>SIGN IN</NavLink>
-        <NavLink className={style.link} to={REGISTER_PATH}>REGISTRATION</NavLink>
-        <NavLink className={style.link} to = {FORGOT_PASSWORD_PATH}>FORGOT PASSWORD</NavLink>
-        <NavLink className={style.link} to = {SET_NEW_PASSWORD_PATH}>SET NEW PASSWORD</NavLink>
-        <NavLink className={style.link} to = {PROFILE_PATH}> PROFILE</NavLink>
-    </div>
+const Header: React.FC<HeaderPropsType> = () => {
 
-}
+	const [disableMenu, setDisableMenu] = useState<boolean>(true);
+
+	return (
+		<div className={style.header}>
+			{disableMenu ?
+				<button onClick={() => setDisableMenu(false)}>Menu</button>
+				:
+				<>
+				<button onClick={() => setDisableMenu(true)}>Menu</button>
+					<NavLink className={style.link} to={SIGN_IN_PATH}>SIGN IN</NavLink>
+					<NavLink className={style.link} to={REGISTER_PATH}>REGISTRATION</NavLink>
+					<NavLink className={style.link} to={FORGOT_PASSWORD_PATH}>FORGOT PASSWORD</NavLink>
+					<NavLink className={style.link} to={SET_NEW_PASSWORD_PATH}>SET NEW PASSWORD</NavLink>
+					<NavLink className={style.link} to={PROFILE_PATH}> PROFILE</NavLink>
+				</>
+			}
+		</div>
+	);
+};
 
 
-export default Header
+export default Header;
