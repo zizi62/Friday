d23d0afe0372d28998470414e8c5be4d8cf109ec
Zoia <zizi2405@gmail.com>
2020-07-04 12:51:28 -0400

    test comit

--- src/components/api/signInApi.ts
+++ src/components/api/signInApi.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
-<<<<<<< HEAD
 
 
-=======
-import {ProfileType} from "../../Redux/profileReducer";
->>>>>>> 2cfa603047737655e37c2de0fc96f3568a22bfb6
 
 const instance = axios.create({
     baseURL: 'https://cards-nya-back.herokuapp.com/1.0/'
@@ -14,7 +10,6 @@ type signInSuccesType = {
     token: string
 }
 
-<<<<<<< HEAD
 type getSighInType =  signInSuccesType & {error: string}
 
 export const signInApi = {   
@@ -31,14 +26,5 @@ export const signInApi = {
          
 //        } 
     }
-=======
-type getProfileType = ProfileType & { error: string }
-
-export const signInApi = {
-    async signIn(email: string, password: string, rememberMe: boolean) {
-        return await instance.post<getProfileType>(`auth/login`, {email, password, rememberMe})
-    }
-};
->>>>>>> 2cfa603047737655e37c2de0fc96f3568a22bfb6
 
 
