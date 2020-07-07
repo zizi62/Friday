992b39d89108dfffbc9c2c0382e0c95bd9cfc919
mityaycho <mityaycho@gmail.com>
2020-07-07 23:28:07 +0300

    add types (:any) from props and ref to tableIcons

--- src/components/table/TableCards.tsx
+++ src/components/table/TableCards.tsx
@@ -18,23 +18,23 @@ import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    Add: forwardRef((props:any, ref:any) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props:any, ref:any) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props:any, ref:any) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props:any, ref:any) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props:any, ref:any) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props:any, ref:any) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props:any, ref:any) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props:any, ref:any) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props:any, ref:any) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props:any, ref:any) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props:any, ref:any) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props:any, ref:any) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props:any, ref:any) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props:any, ref:any) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props:any, ref:any) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props:any, ref:any) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props:any, ref:any) => <ViewColumn {...props} ref={ref} />)
   };
 
 interface Row {
